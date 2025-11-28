dea931b58a6bb18f30892f64f7409dbc92106af3
PH64941 <hmhao2601@gmail.com>
2025-11-28 11:32:46 +0700

    code

--- CodeTaiLop/CodeTaiLop/Program.c
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -2,10 +2,52 @@
 // Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
 
 #include <stdio.h>
-
-int main()
+void kiemTraSoNguyen()
 {
-    printf("Hi there!\n");
+
+}
+void lapChucNang(int chonChucNang)
+{
+    int tiepTuc = 1;
+    while (tiepTuc == 1)
+    {
+        switch (chonChucNang)
+        {
+        case 1: kiemTraSoNguyen();
+            break;
+        case 2: // goi ham o day
+            break;
+        case 0: 
+            return;
+        default: 
+            printf("chon lai [0-3]");
+            break;
+      }
+        printf("Ban co muon tiep tuc khong? (1: Co, 0: Khong): ");
+		scanf_s("%d", &tiepTuc);
+		system("cls"); // clear sreen
+    }
+
+}
+ int main()
+{
+    int chonChucNang;
+    do
+    { 
+        printf("MENU\n");
+		printf("1. Kiem tra so nguyen\n");
+		printf("2. Chuc nang 2\n");
+		printf("0. Thoat\n");
+        scanf_s("%d", &chonChucNang);
+        if (chonChucNang < 0 || chonChucNang > 2)
+        {
+            printf("Chon lai chuc nang!\n");
+        
+		}
+        while (chonChucNang != 0);
+    }
+    
+
 }
 
 // Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
