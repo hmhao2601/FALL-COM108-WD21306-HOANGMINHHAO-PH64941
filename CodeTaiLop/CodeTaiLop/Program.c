59691472a90425d75d2fafb6a9f02f920063e82f
PH64941 <hmhao2601@gmail.com>
2025-12-01 11:03:48 +0700

    chuoi

--- CodeTaiLop/CodeTaiLop/Program.c
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -1,6 +1,4 @@
-﻿// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
-
+﻿
 #include <stdio.h>
 //void kiemTraSoNguyen()
 //{
@@ -48,63 +46,13 @@
 //    }
 //    
 //
-//}
-//  
+////}
 
- /*int main() 
- {
- int y[3] = { 1,2,3 };
- y[1] = 9;
- for (int i = 0; i < 3; i++)
- {
-	 printf("%d ", y[i]);
- }*/
-
- 
- int main() {
-     int n;
-
-     // Nhập số lượng phần tử
-     printf("Nhap so luong phan tu: ");
-     scanf_s("%d", &n);
-
-     // Khai báo mảng với n phần tử
-     int a[n];
-
-     // Nhập giá trị cho từng phần tử
-     for (int i = 0; i < n; i++) {
-         printf("Nhap phan tu a[%d]: ", i);
-         scanf_s("%d", &a[i]);
-     }
-
-     // In các phần tử
-     printf("\nCac phan tu vua nhap: ");
-     for (int i = 0; i < n; i++) {
-         printf("%d ", a[i]);
-     }
-
-     // Tính tổng mảng
-     int tong = 0;
-     for (int i = 0; i < n; i++) {
-         tong += a[i];
-     }
-
-     printf("\nTong cac phan tu = %d\n", tong);
-
-     return 0;
- }
-
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Dung "Solution Explorer window" de them/quan ly cac files
-//   2. Dung "Output window" de kiem tra "build output" va "cac thong bao khac"
-//   3. Dung "Error List window" de xem loi
-//   4. Chon "Project > Add New Item" de tao moi file code,
-//      hoac "Project > Add Existing Item" de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//      chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//      bam phim chuot phai vao "Solution" > chon "Open Folder in File Explorer"
-
-// GV: AnhTT184
+int main()
+{
+	char ten[50];
+	printf("Nhap ten cua ban: ");
+	puts(ten);
+	fgets(ten);
+	printf("Xin chao, %s!\n", ten);
+}
