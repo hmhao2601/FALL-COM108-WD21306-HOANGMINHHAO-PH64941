e76ca9ca8954b7b2ecefd3c4532e34ab33b3b4dd
PH64941 <hmhao2601@gmail.com>
2025-12-01 10:48:40 +0700

    1.12

--- CodeTaiLop/CodeTaiLop/Program.c
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -1,54 +1,98 @@
-// CConsoleApplication.c : file nay chua 'main' function. 
+﻿// CConsoleApplication.c : file nay chua 'main' function. 
 // Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
 
 #include <stdio.h>
-void kiemTraSoNguyen()
-{
+//void kiemTraSoNguyen()
+//{
+//
+//}
+//void lapChucNang(int chonChucNang)
+//{
+//    int tiepTuc = 1;
+//    while (tiepTuc == 1)
+//    {
+//        switch (chonChucNang)
+//        {
+//        case 1: kiemTraSoNguyen();
+//            break;
+//        case 2: // goi ham o day
+//            break;
+//        case 0: 
+//            return;
+//        default: 
+//            printf("chon lai [0-3]");
+//            break;
+//      }
+//        printf("Ban co muon tiep tuc khong? (1: Co, 0: Khong): ");
+//		scanf_s("%d", &tiepTuc);
+//		system("cls"); // clear sreen
+//    }
+//
+//}
+// int main()
+//{
+//    int chonChucNang;
+//    do
+//    { 
+//        printf("MENU\n");
+//		printf("1. Kiem tra so nguyen\n");
+//		printf("2. Chuc nang 2\n");
+//		printf("0. Thoat\n");
+//        scanf_s("%d", &chonChucNang);
+//        if (chonChucNang < 0 || chonChucNang > 2)
+//        {
+//            printf("Chon lai chuc nang!\n");
+//        
+//		}
+//        while (chonChucNang != 0);
+//    }
+//    
+//
+//}
+//  
 
-}
-void lapChucNang(int chonChucNang)
-{
-    int tiepTuc = 1;
-    while (tiepTuc == 1)
-    {
-        switch (chonChucNang)
-        {
-        case 1: kiemTraSoNguyen();
-            break;
-        case 2: // goi ham o day
-            break;
-        case 0: 
-            return;
-        default: 
-            printf("chon lai [0-3]");
-            break;
-      }
-        printf("Ban co muon tiep tuc khong? (1: Co, 0: Khong): ");
-		scanf_s("%d", &tiepTuc);
-		system("cls"); // clear sreen
-    }
+ /*int main() 
+ {
+ int y[3] = { 1,2,3 };
+ y[1] = 9;
+ for (int i = 0; i < 3; i++)
+ {
+	 printf("%d ", y[i]);
+ }*/
 
-}
- int main()
-{
-    int chonChucNang;
-    do
-    { 
-        printf("MENU\n");
-		printf("1. Kiem tra so nguyen\n");
-		printf("2. Chuc nang 2\n");
-		printf("0. Thoat\n");
-        scanf_s("%d", &chonChucNang);
-        if (chonChucNang < 0 || chonChucNang > 2)
-        {
-            printf("Chon lai chuc nang!\n");
-        
-		}
-        while (chonChucNang != 0);
-    }
-    
+ 
+ int main() {
+     int n;
 
-}
+     // Nhập số lượng phần tử
+     printf("Nhap so luong phan tu: ");
+     scanf_s("%d", &n);
+
+     // Khai báo mảng với n phần tử
+     int a[n];
+
+     // Nhập giá trị cho từng phần tử
+     for (int i = 0; i < n; i++) {
+         printf("Nhap phan tu a[%d]: ", i);
+         scanf_s("%d", &a[i]);
+     }
+
+     // In các phần tử
+     printf("\nCac phan tu vua nhap: ");
+     for (int i = 0; i < n; i++) {
+         printf("%d ", a[i]);
+     }
+
+     // Tính tổng mảng
+     int tong = 0;
+     for (int i = 0; i < n; i++) {
+         tong += a[i];
+     }
+
+     printf("\nTong cac phan tu = %d\n", tong);
+
+     return 0;
+ }
 
 // Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
 
---
+++ MangCoDayHo/MangCoDayHo/Program.c
@@ -0,0 +1,25 @@
+﻿
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    int y[10];
+    int tong = 0;
+    printf("Khai bao phan tu: ");
+	scanf_s("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("y[%d]= ", i);
+        scanf_s("%d", &y[i]);
+    }
+
+    printf("Cac phan tu trong mang: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", y[i]);
+		tong += y[i];
+    }
+	printf("\nTong cac phan tu trong mang: %d", tong);
+}
+        
+
+
